0828739be46b48424b41218283e8ed5458cdfe36
Luis Ramos <142616960+LuisRamosPaco@users.noreply.github.com>
2024-04-14 22:18:03 -0400

    Update database.py
    removed Password

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 connection = mysql.connector.connect(
     user='root',
-    password='Qwert12345',
+    password='',
     host='127.0.0.1',
     database='bd_transacciones_lrp')
 
